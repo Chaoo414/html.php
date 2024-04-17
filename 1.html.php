6c4aa13e22896170de49cc2204323c38c1dc5324
Chaoo414 <135246200+Chaoo414@users.noreply.github.com>
2024-04-17 21:27:20 +0800

    Create 1.html.php

---
+++ 1.html.php
@@ -0,0 +1,22 @@
+<html>
+    <head><title>html+php</title></head>      
+    <body>
+        <?php echo "Hello world!"; ?> 
+        <h1>html</h1>
+        <?php echo "Hello world!"; ?> 
+        <h1>html</h1>
+        <?php echo "Hello world!"; ?> 
+    </body>
+</html>
+
+
+<html>: HTML 文件的開始標籤。
+<head><title>html+php</title></head>:題設定為 "html+php"。
+<body>
+<?php echo "Hello world!"; ?>: 出 "Hello world!" 字串。echo 用來輸出內容。
+<h1>html</h1>: 用來定義一個最重要的標題
+<?php echo "Hello world!"; ?>:用來輸出 "Hello world!" 字串。
+<h1>html</h1>: 用來定義一個最重要的標題
+<?php echo "Hello world!"; ?>: 用來輸出 "Hello world!" 字串。
+</body>: HTML 主要內容區塊的結束標籤。
+</html>: HTML 文件的結束標籤。
