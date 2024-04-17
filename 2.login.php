a67907105cac516a859d25f7bb140fc0dd61b499
Chaoo414 <135246200+Chaoo414@users.noreply.github.com>
2024-04-17 21:40:43 +0800

    Create 2.login.php

---
+++ 2.login.php
@@ -0,0 +1,5 @@
+<?php: PHP 程式碼的起始標記。
+echo $_POST["id"];: 使用 echo 來輸出從名為 "id" 的表單欄位中接收到的值，$_POST["id"] 是從 POST 方法提交的表單中名為 "id" 的欄位的值。
+echo "<br>";: 使用 echo 來輸出 HTML 的斷行標籤 <br>，這樣可以讓後續的輸出在網頁中換行顯示。
+echo $_POST["pwd"];: 同樣使用 echo 來輸出從名為 "pwd" 的表單欄位中接收到的值，$_POST["pwd"] 是從 POST 方法提交的表單中名為 "pwd" 的欄位的值。
+?>: PHP 程式碼的結束標記。
